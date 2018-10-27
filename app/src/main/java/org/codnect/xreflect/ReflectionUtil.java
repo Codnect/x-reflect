953879b560346cc620722b56f9d2997023de29bd
Burak Koken <burakkokenn@gmail.com>
2018-10-28 01:53:13 +0200

    Added new methods to get more about classes, its methods and its fields.

--- app/src/main/java/org/codnect/xreflect/ReflectionUtil.java
+++ app/src/main/java/org/codnect/xreflect/ReflectionUtil.java
@@ -1,5 +1,15 @@
 package org.codnect.xreflect;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.WildcardType;
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Created by Burak Koken on 27.10.2018.
  *
@@ -8,13 +18,145 @@ package org.codnect.xreflect;
 public class ReflectionUtil {
 
     /**
+     * Determines if the field is a property.
+     *
+     * @param field the field object
+     * @return if the field is a property, it returns
+     * true. Otherwise it returns false.
+     */
+    public static boolean isProperty(Field field) {
+        return !Modifier.isTransient(field.getModifiers()) &&
+                !Modifier.isStatic(field.getModifiers()) &&
+                !field.isSynthetic() &&
+                !void.class.equals(field.getType());
+    }
+
+    /**
+     * Determines if the method is a property.
+     *
+     * @param method the method object
+     * @return if the method is a property, it returns
+     * true. Otherwise it returns false.
+     */
+    public static boolean isProperty(Method method) {
+        return method.getParameterTypes().length == 0 &&
+                !method.isSynthetic() &&
+                !method.isBridge() &&
+                !Modifier.isStatic(method.getModifiers()) &&
+                !void.class.equals(method.getReturnType()) &&
+                (method.getName().startsWith("get") || method.getName().startsWith("is"));
+    }
+
+    /**
+     * Determines if the type is a simple type.
+     *
+     * @param type an instance of the type
+     * @return if the type is a simple type, it returns
+     * true. Otherwise, it returns false.
+     */
+    public static boolean isSimple(Type type) {
+        if(type instanceof Class) {
+            return !((Class) type).isArray() && !isCollectionClass((Class) type);
+        }
+        else if(type instanceof ParameterizedType) {
+            return isSimple(((ParameterizedType) type).getRawType());
+        }
+        else if(type instanceof WildcardType) {
+            Type[] lowerBounds = ((WildcardType) type).getLowerBounds();
+            for(Type lowerBound : lowerBounds) {
+                if(!isSimple(lowerBound)) {
+                    return false;
+                }
+            }
+            Type[] upperBounds = ((WildcardType) type).getLowerBounds();
+            for(Type upperBound : upperBounds) {
+                if(!isSimple(upperBound)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+
+    }
+
+    /**
+     * Determines if the type is a array type.
+     *
+     * @param type an instance of the type
+     * @return if the type is a array type, it returns
+     * true. Otherwise, it returns false.
+     */
+    public static boolean isArray(Type type) {
+        if(type instanceof Class) {
+            return ((Class) type).isArray();
+        } else if(type instanceof GenericArrayType) {
+            return isSimple(((GenericArrayType) type).getGenericComponentType());
+        }
+
+        return false;
+    }
+
+    /**
+     * Determines if the annotated class is an instance of
+     * the collection class.
+     *
+     * @param annotatedClass the annotated class
+     * @return if the annotated class is an instance of
+     * the collection class, it returns true. Otherwise it
+     * returns false.
+     */
+    public static boolean isCollectionClass(Class annotatedClass) {
+        return Collection.class.isAssignableFrom(annotatedClass)
+                || Map.class.isAssignableFrom(annotatedClass);
+    }
+
+    /**
+     * Get the collection class from type.
+     *
+     * @param type an instance of the type
+     * @return the collection class from type.
+     */
+    public static Class getCollectionClass(Type type) {
+        if(type instanceof Class) {
+            if(isCollectionClass((Class) type)) {
+                return (Class) type;
+            }
+        } else if(type instanceof ParameterizedType) {
+            return getCollectionClass(((ParameterizedType) type).getRawType());
+        } else if(type instanceof WildcardType) {
+            Type[] upperBounds = ((WildcardType) type).getUpperBounds();
+            if(upperBounds.length != 0){
+                return getCollectionClass(upperBounds[0]);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Determines if the type is a collection type.
+     *
+     * @param type an instance of the type
+     * @return if the type is a collection type, it returns
+     * true. Otherwise, it returns false.
+     */
+    public static boolean isCollection(Type type) {
+        if(getCollectionClass(type) != null) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
      * Decapitalizes the property name.
      *
      * @param propertyName the property name
      * @return Decapitalized property name
      */
     public static String decapitalize(String propertyName) {
-
         if (propertyName != null && propertyName.length() != 0) {
             char[] chars = propertyName.toCharArray();
             chars[0] = Character.toLowerCase(chars[0]);
