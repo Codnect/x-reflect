1eedc9b7581d727a9b39518be1ad831d1de61dd1
Burak Koken <burakkokenn@gmail.com>
2018-11-08 23:37:20 +0200

    Added the comments for ReflectionCollector

--- app/src/main/java/org/codnect/xreflect/ReflectionCollector.java
+++ app/src/main/java/org/codnect/xreflect/ReflectionCollector.java
@@ -23,72 +23,80 @@ public class ReflectionCollector {
     }
 
     /**
+     * Add the converted XClass.
      *
-     * @param typePair
-     * @param xClass
+     * @param typePair a type pair
+     * @param xClass an instance of XClass
      */
     public void addXClass(TypePair typePair, XClass xClass) {
        xClassesMap.put(typePair, xClass);
     }
 
     /**
+     * Get the instance of the XClass for specified type pair.
      *
-     * @param typePair
-     * @return
+     * @param typePair a type pair
+     * @return the instance of the XClass for specified type pair
      */
     public XClass getXClass(TypePair typePair) {
         return xClassesMap.get(typePair);
     }
 
     /**
+     * Add the converted XMethod.
      *
-     * @param memberPair
-     * @param xMethod
+     * @param memberPair a member pair
+     * @param xMethod an instance of XMethod
      */
     public void addXMethod(MemberPair memberPair, XMethod xMethod) {
         xMethodsMap.put(memberPair, xMethod);
     }
 
     /**
+     * Get the instance of the XMethod for specified member pair.
      *
-     * @param memberPair
-     * @return
+     * @param memberPair a member pair
+     * @return the instance of the XMethod for specified member pair
      */
     public XMethod getXMethod(MemberPair memberPair) {
         return xMethodsMap.get(memberPair);
     }
 
     /**
+     * Add the converted XField.
      *
-     * @param memberPair
-     * @param xField
+     * @param memberPair a member pair
+     * @param xField an instance of XField
      */
     public void addXField(MemberPair memberPair, XField xField) {
         xFieldsMap.put(memberPair, xField);
     }
 
     /**
+     * Get the instance of the XField for specified member pair.
      *
-     * @param memberPair
-     * @return
+     * @param memberPair a member pair
+     * @return the instance of the XField for specified member pair
      */
     public XField getXField(MemberPair memberPair) {
         return xFieldsMap.get(memberPair);
     }
 
     /**
+     * Add the converted XProperty.
      *
-     * @param memberPair
-     * @param xProperty
+     * @param memberPair a member pair
+     * @param xProperty an instance of XProperty
      */
     public void addXProperty(MemberPair memberPair, XProperty xProperty) {
         xPropertiesMap.put(memberPair, xProperty);
     }
 
     /**
+     * Get the instance of the XProperty for specified member pair.
      *
-     * @param memberPair
-     * @return
+     * @param memberPair a member pair
+     * @return the instance of the XProperty for specified member pair
      */
     public XProperty getXProperty(MemberPair memberPair) {
         return xPropertiesMap.get(memberPair);
