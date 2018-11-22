3e3c8e3409b79cd4fd16d580d497bb61e52097cb
Burak Koken <burakkokenn@gmail.com>
2018-11-23 00:32:16 +0200

    Added the unit tests for XArrayType.

---
+++ app/src/test/java/org/codnect/xreflect/XArrayTypeTest.java
@@ -0,0 +1,64 @@
+package org.codnect.xreflect;
+
+import org.codnect.xreflect.binder.SimpleTypeBinder;
+import org.codnect.xreflect.binder.TypeBinder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Type;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class XArrayTypeTest {
+
+    private ReflectionManager reflectionManager;
+    private XType xType;
+
+    @Before
+    public void initTestObjects() {
+        reflectionManager = new ReflectionManager();
+        XClass fooXClass = reflectionManager.getXClass(Foo.class);
+        XField xField = fooXClass.getDeclaredField("arrayField");
+        TypeBinder typeBinder = SimpleTypeBinder.getInstance();
+        Type type = typeBinder.bind((reflectionManager.toField(xField)).getGenericType());
+        xType = reflectionManager.getXType(typeBinder, type);
+    }
+
+    @Test
+    public void testIsCollection() {
+        assertFalse(xType.isCollection());
+    }
+
+    @Test
+    public void testIsArray() {
+        assertTrue(xType.isArray());
+    }
+
+    @Test
+    public void testGetType() {
+        assertTrue(reflectionManager.equals(xType.getType(), String[].class));
+    }
+
+    @Test
+    public void testGetElementClass() {
+        assertTrue(reflectionManager.equals(xType.getElementClass(), String.class));
+    }
+
+    @Test
+    public void testGetCollectionClass() {
+        assertNull(xType.getCollectionClass());
+    }
+
+    @Test
+    public void testGetMapKey() {
+        assertNull(xType.getMapKey());
+    }
+
+    @Test
+    public void testGetClassOrElementClass() {
+        assertTrue(reflectionManager.equals(xType.getClassOrElementClass(), String.class));
+    }
+
+}
